f350db7985bcbaebcc8a3e7fa377b88caa34e3c3
quanvu <quanvuee@gmail.com>
2022-01-17 16:01:05 +0700

    update menu-item withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage/hompage.component';
+import React from "react";
+import "./App.css";
+import Homepage from "./pages/homepage/hompage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
